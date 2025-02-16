3e6ca80711c00d13adeda898015c90c0c5834652
lizyends <90591814+lizyends@users.noreply.github.com>
2025-02-16 10:55:18 +0800

    Create DSEBench.sh

---
+++ code/DSEBench.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run.py --dataset DSEBench --len-seq 10 --num-seqs 10 --dropout 0.1 --beta 0.1 --temperature 0.5 --lr 5e-5 --epoch 10 --feats-type 0 --mode qc --num-gnns 1 --batch-size 128 --patience 1 --num-layers 3 --eval-steps 500 --num-heads 4 --top-k 5
