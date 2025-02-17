2dc49615cf5246d3673e02a1c20cfeafafa59f6e
HeJing <lizyends@163.com>
2025-02-17 21:10:41 +0800

    update .sh

--- code/DataFinder.sh
+++ code/DataFinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python run.py --dataset DataFinder --len-seq 10 --num-seqs 10  --dropout 0.1 --beta 0.1 --temperature 0.2 --lr 1e-4 --epoch 50 --feats-type 0 --num-gnns 3 --batch-size 128 --patience 5 --num-layers 3 --weight-decay 1e-4 --eval-steps 200 --num-heads 4 --repeat 5 --top-k 5
+CUDA_VISIBLE_DEVICES=0 python run.py --dataset DataFinder --len-seq 10 --num-seqs 10  --dropout 0.1 --beta 0.1 --temperature 0.2 --lr 1e-4 --epoch 50 --feats-type 0 --num-gnns 3 --batch-size 128 --patience 5 --num-layers 3 --weight-decay 1e-4 --eval-steps 200 --num-heads 4 --top-k 5
